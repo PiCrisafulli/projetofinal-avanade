62c2c6f21f72b0079f4e56143b57a5add1eebe68
Pi Crisafulli <prijett@hotmail.com>
2018-11-05 18:21:00 -0200

    Estrutura Inicial do Projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ConfigComponent } from './config/config.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+
+const appRoutes: Routes = [{
+  path:'', component: HomeComponent,},
+  {path: 'register', component: RegisterComponent,},
+  {path: 'login',  component: LoginComponent,
+}];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavComponent,
+    ProfileComponent,
+    PostsComponent,
+    HomeComponent,
+    FooterComponent,
+    SearchComponent,
+    ConfigComponent,
+    HeaderComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
